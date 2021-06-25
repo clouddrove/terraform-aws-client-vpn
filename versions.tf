1479251180159dfc35d112e567bfc8ff20a8e684
HarmanJyot Kaur <harman@clouddrove.com>
2021-06-25 16:16:43 +0530

    updated version constrain

--- versions.tf
+++ versions.tf
@@ -1,12 +1,9 @@
 # Terraform version
 terraform {
-  required_version = ">= 0.12.0"
-
+  required_version = ">= 0.14"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.1.15"
+      source = "hashicorp/aws"
     }
   }
 }
-
