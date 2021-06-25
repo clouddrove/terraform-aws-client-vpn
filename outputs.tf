5bf2e0ed71113b01ae25067b840e24ed70cf811f
Anmol Nagpal <anmol@clouddrove.com>
2021-06-25 18:16:27 +0530

    add terratest and readme

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "cert" {
   description = "A mapping of tags to assign to the certificate."
   sensitive   = true
 }
+
+output "tags" {
+  value       = module.labels.tags
+  description = "A mapping of tags to assign to the resource."
+}
