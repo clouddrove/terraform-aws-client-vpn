31401440b80fa48e94809d4419e1b2799eb6151b
Anmol <anmol.nagpal@deepimmo.com>
2022-11-02 20:21:57 +0530

    fixing error

--- main.tf
+++ main.tf
@@ -145,12 +145,12 @@ resource "aws_ec2_client_vpn_endpoint" "default" {
   }
 
   tags = module.labels.tags
-  authentication_options {
-    type                            = var.type
-    saml_provider_arn               = var.saml_arn
-    self_service_saml_provider_arn  = var.self_saml_arn
-    root_certificate_chain_arn = join("", aws_acm_certificate.root.*.arn)
+  lifecycle {
+    ignore_changes = [
+      authentication_options
+    ]
   }
+
 
 }
 
