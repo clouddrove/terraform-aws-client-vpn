b6b320baf702da06b5577735089b5344def1afbf
mamraj yadav <80300065+mamrajyadav@users.noreply.github.com>
2023-09-07 20:13:42 +0530

    update main.tf, version.tf and github action and add locals in e… (#20)
    * fix: update main.tf, version.tf and github action and add locals in example
    
    * fix: fix security group issue
    
    * fix: run terraform fmt
    
    * fix: dfsec issue
    
    * fix: dfsec issue
    
    * fix: update vpn subnet and add lifecycle in acm
    
    * feat: added tls provider and veriables description
    
    * fix: remove tls provider and add in version.tf

--- _example/example.tf
+++ _example/example.tf
@@ -1,8 +1,10 @@
-##---------------------------------------------------------------------------------------------------------------------------
-## Provider block added, Use the Amazon Web Services (AWS) provider to interact with the many resources supported by AWS.
-##--------------------------------------------------------------------------------------------------------------------------
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
+}
+
+locals {
+  name        = "vpn"
+  environment = "test"
 }
 
 ##---------------------------------------------------------------------------------------------------------------------------
@@ -12,33 +14,30 @@ module "vpc" {
   source  = "clouddrove/vpc/aws"
   version = "2.0.0"
 
+  name            = local.name
+  environment     = local.environment
   enable_flow_log = false
-  name            = "vpc"
-  environment     = "example"
-  label_order     = ["name", "environment"]
-
-  cidr_block = "10.0.0.0/16"
+  cidr_block      = "10.0.0.0/16"
 }
 
 ##-----------------------------------------------------
 ## A subnet is a range of IP addresses in your VPC.
 ##-----------------------------------------------------
+#tfsec:ignore:aws-ec2-no-excessive-port-access
+#tfsec:ignore:aws-ec2-no-public-ingress-acl
 module "subnets" {
   source  = "clouddrove/subnet/aws"
   version = "2.0.0"
 
   nat_gateway_enabled = true
-
-  name        = "subnets"
-  environment = "example"
-  label_order = ["name", "environment"]
-
-  availability_zones = ["eu-west-1a", "eu-west-1b"]
-  vpc_id             = module.vpc.vpc_id
-  type               = "public-private"
-  igw_id             = module.vpc.igw_id
-  cidr_block         = module.vpc.vpc_cidr_block
-  ipv6_cidr_block    = module.vpc.ipv6_cidr_block
+  name                = local.name
+  environment         = local.environment
+  availability_zones  = ["us-east-1a", "us-east-1b"]
+  vpc_id              = module.vpc.vpc_id
+  type                = "public-private"
+  igw_id              = module.vpc.igw_id
+  cidr_block          = module.vpc.vpc_cidr_block
+  ipv6_cidr_block     = module.vpc.ipv6_cidr_block
 }
 
 ##-----------------------------------------------------------------------------
@@ -47,16 +46,13 @@ module "subnets" {
 module "vpn" {
   source = "../"
 
-  name                = "test-vpn"
-  enabled             = true
+  name                = local.name
+  environment         = local.environment
   split_tunnel_enable = true
-  environment         = "example"
-  label_order         = ["name", "environment"]
   cidr_block          = "172.0.0.0/16"
+  vpc_id              = module.vpc.vpc_id
   subnet_ids          = module.subnets.public_subnet_id
   route_cidr          = ["0.0.0.0/0", "0.0.0.0/0"]
-  security_group_ids  = [""]
   route_subnet_ids    = module.subnets.public_subnet_id
   network_cidr        = ["0.0.0.0/0"]
-
 }
--- versions.tf
+++ versions.tf
@@ -1,11 +1,15 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.9.0"
+      version = ">= 5.13.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.4"
     }
   }
 }
