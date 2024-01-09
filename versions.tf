069640489499c94dd309d4ff861a089ad56bdced
theprashantyadav <theprashantyadav7@gmail.com>
2024-01-09 13:58:13 +0530

    feat: update tf.checks step name and tf and aws version

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.5.5"
+  required_version = ">= 1.6.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.13.1"
+      version = ">= 5.31.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.0.4"
+      version = ">= 4.0.5"
     }
   }
 }
